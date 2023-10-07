d24919854d0013fbc7c81406ca5c95ca00723f3f
famousyub <famousmeyub@gmail.com>
2023-10-07 14:26:45 +0100

    adminatys

---
+++ src/app/model/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+  id: number;
+  start: any;
+  end: any;
+  title: string;
+  fieldId: number;
+  total: number;
+  customerId: number;
+  primaryColor: string;
+  secondaryColor: string;
+}
